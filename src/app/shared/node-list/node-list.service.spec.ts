a5c963070f160b18536170eb443831ef617497cc
Arne Hassel <arne.hassel@gmail.com>
2016-12-27 22:57:17 +0100

    Added a simple test for NodeListService

--- src/app/shared/node-list/node-list.service.spec.ts
+++ src/app/shared/node-list/node-list.service.spec.ts
@@ -1,3 +1,17 @@
+import { NodeListService } from './node-list.service';
+import {
+  async,
+  getTestBed,
+  TestBed
+} from '@angular/core/testing';
+
 describe('NodeListService', () => {
-  it('converts nodeList to arrays', () => expect(true).toBe(true));
+  let service;
+
+  beforeEach(() => {
+    service = new NodeListService();
+  });
+
+  it('converts empty nodeList to an empty array', () => expect(service.toArray({length: 0})).toEqual([]));
+  it('converts nodeList with children to populated array', () => expect(service.toArray({length: 2, 0: 0, 1: 1})).toEqual([0, 1]));
 });
